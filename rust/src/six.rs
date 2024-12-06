4cf146ffa41224255e76b643c4141ea14cad77fa
Chris <chris@spinque.com>
2024-12-06 22:37:03 +0100

    88ms :)

--- rust/src/six.rs
+++ rust/src/six.rs
@@ -4,10 +4,10 @@ use crate::utils::data_path;
 
 #[derive(Eq, Hash, PartialEq, Clone, Copy)]
 enum Direction {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST,
+    NORTH = 0,
+    EAST = 1,
+    SOUTH = 2,
+    WEST = 3,
 }
 
 impl Direction {
@@ -19,9 +19,19 @@ impl Direction {
             Direction::WEST => Direction::NORTH,
         }
     }
+
+    fn to_int(&self) -> i32
+    {
+        match self {
+            Direction::NORTH => 0,
+            Direction::EAST => 1,
+            Direction::SOUTH => 2,
+            Direction::WEST => 3,
+        }
+    }
 }
 
-fn next_location(location: (i16, i16), direction: Direction) -> (i16, i16) {
+fn next_location(location: (i32, i32), direction: Direction) -> (i32, i32) {
     match direction {
         Direction::NORTH => (location.0 - 1, location.1),
         Direction::EAST  => (location.0, location.1 + 1),
@@ -32,8 +42,8 @@ fn next_location(location: (i16, i16), direction: Direction) -> (i16, i16) {
 
 pub fn main() {
     let mut obstructions = [false; 130 * 130];
-    let mut guard_start: (i16, i16) = (0, 0);
-    let mut visited: HashSet<(i16, i16)> = HashSet::with_capacity(130*130);
+    let mut guard_start: (i32, i32) = (0, 0);
+    let mut visited: HashSet<(i32, i32)> = HashSet::with_capacity(130*130);
 
     let data = fs::read_to_string(data_path("6.txt")).expect("Something went wrong reading the file");
     let rows = data.split_whitespace();
@@ -42,7 +52,9 @@ pub fn main() {
             match cell {
                 '.' => {},
                 '#' => obstructions[y*130 + x] = true,
-                '^' => guard_start = (y as i16, x as i16),
+                '^' => guard_start = (y as i32
+                                      , x as i32
+                ),
                 _ => panic!("Should not be possible")
             }
         }
@@ -67,7 +79,8 @@ pub fn main() {
     println!("{:?}", visited.len());
 
     visited.remove(&guard_start);
-    let mut counter:i16 = 0;
+    let mut counter:i32
+        = 0;
     for v in visited {
         obstructions[(v.0 * 130 + v.1) as usize] = true;
         let g = guard_start;
@@ -79,24 +92,27 @@ pub fn main() {
     println!("{:?}", counter);
 }
 
-fn is_loop(_guard: (i16, i16), _dir: Direction, _obstacles: [bool; 130 * 130]) -> bool {
+fn is_loop(_guard: (i32
+                    , i32
+), _dir: Direction, _obstacles: [bool; 130 * 130]) -> bool {
     let mut _dir = _dir;
     let mut _guard = _guard;
-    let mut visited: HashSet<(i16, i16, Direction)> = HashSet::with_capacity(130*130*4);
-    visited.insert((_guard.0, _guard.1, _dir));
+    let mut visited = [false; 130*130*4];
+    visited[(_guard.0 * 130 * 4 + _guard.1 * 4 + _dir.to_int()) as usize] = true;
+
     loop {
-        let new_loc = next_location(_guard, _dir);
-        if visited.contains(&(new_loc.0, new_loc.1, _dir)) {
-            return true;
-        }
-        else if new_loc.0 < 0 || new_loc.0 > 129 || new_loc.1 < 0 || new_loc.1 > 129 {
+        let new_loc = next_location(_guard, _dir);    // let mut obstructions: HashSet<(u8, u8)> = HashSet::with_capacity(130*130);
+        if new_loc.0 < 0 || new_loc.0 > 129 || new_loc.1 < 0 || new_loc.1 > 129 {
             return false;
+        }
+        else if visited[(new_loc.0 * 130 * 4 + new_loc.1 * 4 + _dir.to_int()) as usize] {
+            return true;
         }
         else if _obstacles[(new_loc.0 * 130 + new_loc.1) as usize] {
             _dir = _dir.next();
         }
         else {
-            visited.insert((new_loc.0, new_loc.1, _dir));
+            visited[(new_loc.0 * 130 * 4 + new_loc.1 * 4 + _dir.to_int()) as usize] = true;
             _guard = new_loc;
         }
     }
