917e80a271406b8197f0f9863f2ea606ead0d99e
Chris <chris@spinque.com>
2024-12-06 21:38:34 +0100

    Finally under a second

--- rust/src/six.rs
+++ rust/src/six.rs
@@ -21,54 +21,28 @@ impl Direction {
     }
 }
 
-fn next_location(location: (u8, u8), direction: Direction) -> (u8, u8) {
+fn next_location(location: (i16, i16), direction: Direction) -> (i16, i16) {
     match direction {
-        Direction::NORTH => {
-            if location.0 == 0 {
-                return (255, 255)
-            }
-            (location.0 - 1, location.1)
-        }
-        Direction::EAST => {
-            if location.1 == 129 {
-                return (255, 255)
-            }
-            (location.0, location.1 + 1)
-        },
-        Direction::SOUTH => {
-            if location.0 == 129 {
-                return (255, 255)
-            }
-            (location.0 + 1, location.1)
-        },
-        Direction::WEST => {
-            if location.1 == 0 {
-                return (255, 255)
-            }
-            (location.0, location.1 - 1)
-        },
+        Direction::NORTH => (location.0 - 1, location.1),
+        Direction::EAST  => (location.0, location.1 + 1),
+        Direction::SOUTH => (location.0 + 1, location.1),
+        Direction::WEST  => (location.0, location.1 - 1)
     }
 }
 
 pub fn main() {
-    let mut obstructions: HashSet<(u8, u8)> = HashSet::new();
-    let mut guard_start: (u8, u8) = (0, 0);
-    let mut visited: HashSet<(u8, u8)> = HashSet::with_capacity(130*130);
+    let mut obstructions = [false; 130 * 130];
+    let mut guard_start: (i16, i16) = (0, 0);
+    let mut visited: HashSet<(i16, i16)> = HashSet::with_capacity(130*130);
 
     let data = fs::read_to_string(data_path("6.txt")).expect("Something went wrong reading the file");
     let rows = data.split_whitespace();
     for (y, row) in rows.enumerate() {
         for (x, cell) in row.chars().enumerate() {
             match cell {
-                '.' => continue,
-                '#' => {
-                    obstructions.insert((y as u8, x as u8));
-                    continue
-                },
-                '^' => {
-                    guard_start = (y as u8, x as u8);
-                    continue
-                }
+                '.' => {},
+                '#' => obstructions[y*130 + x] = true,
+                '^' => guard_start = (y as i16, x as i16),
                 _ => panic!("Should not be possible")
             }
         }
@@ -79,10 +53,10 @@ pub fn main() {
     visited.insert(guard);
     loop {
         let nl = next_location(guard, direction);
-        if nl == (255, 255) {
+        if nl.0 < 0 || nl.1 < 0 || nl.0 > 129 || nl.1 > 129 {
             break;
         }
-        else if obstructions.contains(&nl) {
+        else if obstructions[(nl.0 * 130 + nl.1) as usize] {
             direction = direction.next();
         }
         else {
@@ -95,30 +69,30 @@ pub fn main() {
     visited.remove(&guard_start);
     let mut counter:i16 = 0;
     for v in visited {
-        obstructions.insert(v);
+        obstructions[(v.0 * 130 + v.1) as usize] = true;
         let g = guard_start;
-        if is_loop(g, Direction::NORTH, obstructions.clone()) {
+        if is_loop(g, Direction::NORTH, obstructions) {
             counter += 1;
         }
-        obstructions.remove(&v);
+        obstructions[(v.0 * 130 + v.1) as usize] = false;
     }
     println!("{:?}", counter);
 }
 
-fn is_loop(_guard: (u8, u8), _dir: Direction, _obstacles: HashSet<(u8, u8)>) -> bool {
+fn is_loop(_guard: (i16, i16), _dir: Direction, _obstacles: [bool; 130 * 130]) -> bool {
     let mut _dir = _dir;
     let mut _guard = _guard;
-    let mut visited: HashSet<(u8, u8, Direction)> = HashSet::with_capacity(130*130*4);
+    let mut visited: HashSet<(i16, i16, Direction)> = HashSet::with_capacity(130*130*4);
     visited.insert((_guard.0, _guard.1, _dir));
     loop {
         let new_loc = next_location(_guard, _dir);
         if visited.contains(&(new_loc.0, new_loc.1, _dir)) {
             return true;
         }
-        else if new_loc == (255, 255) {
+        else if new_loc.0 < 0 || new_loc.0 > 129 || new_loc.1 < 0 || new_loc.1 > 129 {
             return false;
         }
-        else if _obstacles.contains(&new_loc) {
+        else if _obstacles[(new_loc.0 * 130 + new_loc.1) as usize] {
             _dir = _dir.next();
         }
         else {
