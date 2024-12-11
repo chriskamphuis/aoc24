8e83af725024c72dee35b03eb6065c0fdcc285fe
Chris <chris@spinque.com>
2024-12-11 08:01:20 +0100

    w/o build in

--- python/eleven.py
+++ python/eleven.py
@@ -1,24 +1,20 @@
 stones = [int(e) for e in open('../data/11.txt').read().strip().split()]
-
+store = dict()
 def blink(stone, depth):
-    store = dict()
-    def inner(stone, depth):
-        if (stone, depth) in store:
-            return store[(stone, depth)]
+    if (stone, depth) in store:
+        return store[(stone, depth)]
+    else:
+        if depth == 0:
+            return 1
         else:
-            if depth == 0:
-                return 1
+            if stone == 0:
+                store[(stone, depth)] = blink(1, depth - 1)
+            elif (l := len(str(stone))) % 2 == 0:
+                sep = 10 ** l // 2
+                store[(stone, depth)] = blink(stone//sep, depth - 1) + blink(stone%sep, depth - 1)
             else:
-                if stone == 0:
-                    store[(stone, depth)] = inner(1, depth - 1)
-                elif (l := len(str(stone))) % 2 == 0:
-                    sep = 10 ** l // 2
-                    store[(stone, depth)] = inner(stone//sep, depth - 1) + inner(stone%sep, depth - 1)
-                else:
-                    store[(stone, depth)] = inner(stone * 2024, depth - 1)
-                return store[(stone, depth)]
-
-    return inner(stone, depth)
+                store[(stone, depth)] = blink(stone * 2024, depth - 1)
+            return store[(stone, depth)]
 
 print(sum([blink(s, 25) for s in stones]))
 print(sum([blink(s, 75) for s in stones]))
