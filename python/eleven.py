4715b57b58b88b59e33da66dbcf4ed9fda6ef068
Chris <chris@spinque.com>
2024-12-11 08:07:13 +0100

    w/o build in

--- python/eleven.py
+++ python/eleven.py
@@ -3,18 +3,16 @@ store = dict()
 def blink(stone, depth):
     if (stone, depth) in store:
         return store[(stone, depth)]
+    if depth == 0:
+        return 1
+    if stone == 0:
+        store[(stone, depth)] = blink(1, depth - 1)
+    elif (l := len(str(stone))) % 2 == 0:
+        sep = 10 ** l // 2
+        store[(stone, depth)] = blink(stone//sep, depth - 1) + blink(stone%sep, depth - 1)
     else:
-        if depth == 0:
-            return 1
-        else:
-            if stone == 0:
-                store[(stone, depth)] = blink(1, depth - 1)
-            elif (l := len(str(stone))) % 2 == 0:
-                sep = 10 ** l // 2
-                store[(stone, depth)] = blink(stone//sep, depth - 1) + blink(stone%sep, depth - 1)
-            else:
-                store[(stone, depth)] = blink(stone * 2024, depth - 1)
-            return store[(stone, depth)]
+        store[(stone, depth)] = blink(stone * 2024, depth - 1)
+    return store[(stone, depth)]
 
 print(sum([blink(s, 25) for s in stones]))
 print(sum([blink(s, 75) for s in stones]))
